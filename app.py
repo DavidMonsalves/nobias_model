ee6a5be9866634711bca12ed8eaffbcd497ddcd6
David Monsalves <34013352+DavidMonsalves@users.noreply.github.com>
2021-09-29 12:05:54 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import pickle
 
 modelG = pickle.load(open('dt_model_PG_nobias.pkl', 'rb'))
 modelAttribution = pickle.load(open('dt_model_PAttribution_nobias.pkl', 'rb'))
+modelPerformance = pickle.load(open('dt_model_PPerformance_nobias.pkl', 'rb'))
 
 app = Flask(__name__)
 
@@ -30,6 +31,16 @@ def home2():
         pre = np.array(values)
         pre = pre.reshape(1, -1)
         res = modelAttribution.predict(pre)
+        return str(res[0])
+
+@app.route('/predictPerformance', methods = ['GET', 'POST'])
+def home3():
+    if request.method == 'POST':
+        event = json.loads(request.data)
+        values = event['values']
+        pre = np.array(values)
+        pre = pre.reshape(1, -1)
+        res = modelPerformance.predict(pre)
         return str(res[0])
 
 if __name__ == '__main__':
