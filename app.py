d52f97a46084223b4860631926d7bd6fe7cc96e7
David Monsalves <34013352+DavidMonsalves@users.noreply.github.com>
2021-11-22 15:54:59 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ modelG = pickle.load(open('dt_model_PG_nobias.pkl', 'rb'))
 modelAttribution = pickle.load(open('dt_model_PAttribution_nobias.pkl', 'rb'))
 modelPerformance = pickle.load(open('dt_model_PPerformance_nobias.pkl', 'rb'))
 modelUnconscious = pickle.load(open('dt_model_PUnconscious_nobias.pkl', 'rb'))
+modelMaternal = pickle.load(open('dt_model_PMaternal_nobias.pkl', 'rb'))
 
 app = Flask(__name__)
 CORS(app)
@@ -54,6 +55,16 @@ def home4():
         pre = np.array(values)
         pre = pre.reshape(1, -1)
         res = modelUnconscious.predict(pre)
+        return str(res[0])
+
+@app.route('/predictMaternal', methods = ['GET', 'POST'])
+def home5():
+    if request.method == 'POST':
+        event = json.loads(request.data)
+        values = event['values']
+        pre = np.array(values)
+        pre = pre.reshape(1, -1)
+        res = modelMaternal.predict(pre)
         return str(res[0])
 
 if __name__ == '__main__':
